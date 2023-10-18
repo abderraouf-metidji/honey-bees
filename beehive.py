ccffc3874192374868cab93a45659cbbce171fec
abderraouf-metidji <115145685+abderraouf-metidji@users.noreply.github.com>
2023-10-18 18:45:34 +0200

    Script for the bee generations

--- beehive.py
+++ beehive.py
@@ -4,15 +4,24 @@ import csv
 
 class Flower:
     def __init__(self, x, y):
+        """
+        Initializes a Flower object with x and y coordinates.
+        """
         self.x, self.y = x, y
         self.flowers = []
 
     def distance(self, other_flower):
-        """Calculate distance between two flowers"""
+        """
+        Calculates the Euclidean distance between two flowers.
+        """
         return ((float(self.x) - float(other_flower.x))**2 + (float(self.y) - float(other_flower.y))**2)**0.5
 
     def flower_distance(self):
-        """Calculate distance between all flowers"""
+        """
+        Reads flower coordinates from a CSV file and creates a list of Flower objects.
+        Returns:
+            list: A list of Flower objects representing the flowers.
+        """
         flowers = []
         with open('flower_coordinates.csv', 'r', newline='') as csvfile:
             reader = csv.reader(csvfile)
@@ -20,113 +29,92 @@ class Flower:
                 x, y = row[0], row[1]
                 flowers.append(Flower(float(x), float(y)))  # Convert x and y to floats
         self.flowers = flowers  # Assign the list of flowers to self.flowers
-
         return flowers
 
     def matrix(self):
-        """Create a matrix with all distances between flowers"""
+        """
+        Creates a matrix with all distances between flowers.
+        Returns:
+            list: A matrix containing the distances between flowers.
+        """
         distance_matrix = [[flower.distance(other_flower) for other_flower in self.flowers] for flower in self.flowers]
         return distance_matrix
 
-    # def print_matrix(self):
-    #     """Print the matrix"""
-    #     distance_matrix = self.matrix()
-    #     for row in distance_matrix:
-    #         rounded_row = [round(element, 1) for element in row]  # Round each element in the row
-    #         print(rounded_row)
 
 class Beehive(Flower):
     def __init__(self, x, y, flowers):
+        """
+        Initializes a Beehive object with a position and a list of flowers.
+        """
         super().__init__(x, y)
         self.flowers = flowers
         self.distance_matrix = self.matrix()  # Calculate the distance matrix
         self.genome_list = self.butiner(101)  # Create a list of 100 bees
 
     def butiner(self, num_iterations):
-        """Managing the foraging of the bees"""
+        """
+        Manages the foraging behavior of the bees.
+        Returns:
+            list: A list of tuples representing the genomes and distances traveled by the bees.
+        """
         genome_list = []
-
         for _ in range(num_iterations - 1):  # Run for num_iterations - 1 times to generate 100 genomes
             remaining_flowers = self.flowers.copy()  # Create a copy of the list of flowers
             bee_genome = []
             bee_distance = 0
-
             while remaining_flowers:
                 random_flower = random.choice(remaining_flowers)  # Choose a random flower from the remaining ones
                 bee_genome.append(random_flower)  # Add the random flower to the bee's genome list
-
                 # Calculate the distance using the matrix
                 if len(bee_genome) > 1:
                     prev_flower = bee_genome[-2]
                     bee_distance += self.distance_matrix[self.flowers.index(prev_flower)][self.flowers.index(random_flower)]
-
                 remaining_flowers.remove(random_flower)  # Remove the chosen flower from the remaining flowers
-
             genome_list.append((bee_genome, bee_distance))
-
         return genome_list
-    
-    
+
     def selection(self):
+        """
+        Selects the best-performing bees based on the distance traveled.
+        Returns:
+            list: The list of the best-performing bees.
+        """
         self.genome_list.sort(key=lambda x: x[1])  # Sort the list by distance
         self.genome_list = self.genome_list[:50]  # Keep the 50 best bees
+        return self.genome_list
 
     def reproduction(self):
-        # Create a list of available parents from the 50 bees that were not removed
-        available_parents = self.genome_list[:50]
-
-        # Check if there are enough available parents to perform reproduction
-        while len(self.genome_list) < 100 and len(available_parents) >= 2:
-            # Randomly select 2 parents from the list
-            parent_a = choice(available_parents)
+        """
+        Performs reproduction by creating offspring from the selected parents.
+        Returns:
+            list: The updated list of genomes after reproduction.
+        """
+        available_parents = self.genome_list[:50]  # Create a list of available parents from the 50 bees that were not removed
+        while len(self.genome_list) < 100 and len(available_parents) >= 2:  # Check if there are enough available parents to perform reproduction
+            parent_a = choice(available_parents)  # Randomly select 2 parents from the list
             available_parents.remove(parent_a)
-
-            # Filter available parents that don't have the same flower in their genome list
-            valid_parents = [parent for parent in available_parents if not any(flower in parent[0] for flower in parent_a[0])]
-            if not valid_parents:
-                break  # Break the loop if there are no valid parents remaining
-
-            parent_b = choice(valid_parents)
+            parent_b = choice(available_parents)
             available_parents.remove(parent_b)
-
-            # Calculate the pivot point
-            pivot = len(parent_a[0]) // 3
-
-            # Create children based on the pivot
-            child1_genome = parent_a[0][:pivot] + parent_b[0][pivot:]
+            pivot = len(parent_a[0]) // 2  # Calculate the pivot point
+            child1_genome = parent_a[0][:pivot] + parent_b[0][pivot:]  # Create children based on the pivot
             child2_genome = parent_b[0][:pivot] + parent_a[0][pivot:]
-
-            # Calculate the distances for the children
-            child1_distance = self.calculate_distance(child1_genome)
+            child1_distance = self.calculate_distance(child1_genome)  # Calculate the distances for the children
             child2_distance = self.calculate_distance(child2_genome)
-
-            # Add the children to the Beehive
-            self.genome_list.append((child1_genome, child1_distance))
-            self.genome_list.append((child2_genome, child2_distance))
-
-            
-    # def print_genome_list(self):
-    #     count = 0
-    #     for genome, distance in self.genome_list:
-    #         count += 1
-    #         flower_coordinates = [(flower.x, flower.y) for flower in genome]
-    #         print(f'Bee {count} - Distance: {distance}')
+            if len(self.genome_list) < 100:  # Check if the number of genomes is less than 100
+                self.genome_list.append((child1_genome, child1_distance))
+            if len(self.genome_list) < 100:  # Check if the number of genomes is less than 100
+                self.genome_list.append((child2_genome, child2_distance))
+        return self.genome_list
 
     def calculate_distance(self, genome):
-        """Calculate the distance for a given genome"""
+        """
+        Calculates the total distance traveled for a given genome.
+        Returns:
+            float: The total distance traveled by the bee.
+        """
         distance = 0
         for i in range(1, len(genome)):
             prev_flower = genome[i - 1]
             curr_flower = genome[i]
             distance += self.distance_matrix[self.flowers.index(prev_flower)][self.flowers.index(curr_flower)]
         return distance
-
-
-# if __name__ == '__main__':
-    # flower = Flower(0, 0)
-    # flowers = flower.flower_distance()
-    # beehive = Beehive(0, 0, flowers)
-    # beehive.selection()
-    # beehive.reproduction()
-    # flower.print_matrix()
-    # beehive.print_genome_list()
--- main.py
+++ main.py
@@ -1,33 +1,63 @@
-from beehive import * 
+import os
+from beehive import Flower, Beehive
 import random
+import statistics
+import matplotlib.pyplot as plt
 
 class Generation(Beehive):
-    def __init__(self, x, y, flowers):
+    def __init__(self, x, y, flowers, num_generations=25):
+        """
+        Initializes a new generation of bees.
+        """
         super().__init__(x, y, flowers)
-        
-        generation = 0
-        avg_distances = []
-        
-        while generation < 10:
-            self.genome_list = self.butiner(101)
-            self.genome_list.sort(key=lambda x: x[1])
-            self.genome_list = self.genome_list[:100]
-            generation += 1
-            
-            avg_distance = sum(distance for _, distance in self.genome_list) / len(self.genome_list)
-            avg_distances.append(avg_distance)
-            
-            print(f"Generation {generation}: Average distance: {avg_distance}")
+        self.avg_distances = []
+        self.generation = 0
+        self.genome_list = []
 
-    def print_genome_list(self):
-        count = 0
-        for genome, distance in self.genome_list:
-            count += 1
-            flower_coordinates = [(flower.x, flower.y) for flower in genome]
-            print(f'Bee {count} - Distance: {distance}')
+        while self.generation < num_generations:
+            if self.generation == 0:
+                self.genome_list = self.butiner(len(flowers))
+            else:
+                self.selection()
+                self.reproduction()
+
+            self.generation += 1
+            avg_distance = round(statistics.mean(distance for _, distance in self.genome_list), 2)
+            self.avg_distances.append(avg_distance)
+            print("Generation {}: Average distance: {}".format(self.generation, avg_distance))
+
+    def save_graphs(self):
+        """
+        Saves the best bee path and average distance evolution graphs.
+        Creates a 'graphs' folder if it doesn't exist and saves the graphs in that folder.
+        """
+        if not os.path.exists('graphs'):
+            os.makedirs('graphs')
+
+        best_bee_genome, _ = min(self.genome_list, key=lambda x: x[1])
+        x_values = [flower.x for flower in best_bee_genome]
+        y_values = [flower.y for flower in best_bee_genome]
+
+        plt.figure()
+        plt.plot(x_values, y_values, marker='o', linestyle='-', color='b')
+        plt.plot(x_values[0], y_values[0], marker='o', color='g')  # Starting point in green
+        plt.plot(x_values[-1], y_values[-1], marker='o', color='r')  # Ending point in red
+        plt.title('Best Bee Path')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.savefig('graphs/best_bee_path.png')
+        plt.show()
+
+        plt.figure()
+        plt.plot(range(1, len(self.avg_distances) + 1), self.avg_distances, marker='o', linestyle='-', color='r')
+        plt.title('Evolution of Average Distance')
+        plt.xlabel('Generation')
+        plt.ylabel('Average Distance')
+        plt.savefig('graphs/average_distance_evolution.png')
+        plt.show()
 
 if __name__ == '__main__':
     flower = Flower(0, 0)
     flowers = flower.flower_distance()
     generation = Generation(0, 0, flowers)
-    generation.print_genome_list()
+    generation.save_graphs()
